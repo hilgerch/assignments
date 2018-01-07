39ee658a13f6ce11f0e3ee214680adb5efa4b671
Chris Hilger <hilgerch@gmail.com>
2018-01-07 00:19:05 -0500

    add functions to calculator

--- calculator.js
+++ calculator.js
@@ -1,20 +1,25 @@
-def add(a, b) {
+function add(a, b) {
 
 	return a+b
 }
 
-def subtract(a, b) {
+function  subtract(a, b) {
 
 	return a - b
 }
 
-
-def multiply(a, b) {
+function  multiply(a, b) {
 
 	return a * b
 }
 
-def divide(a, b) {
+function  divide(a, b) {
 
 	return a/b
 }
+
+
+
+console.log("I'm going use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
+console.log(x)
